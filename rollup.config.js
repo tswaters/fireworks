6c01c0f6916d60ede3942454cf732f73b94b63aa
Tyler Waters <tyler.waters@gmail.com>
2020-01-04 15:02:11 -0800

    Refactor: use start script, build/serve html; consistent examples

--- rollup.config.js
+++ rollup.config.js
@@ -3,49 +3,59 @@ import resolve from 'rollup-plugin-node-resolve'
 import {terser} from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript'
 import copy from 'rollup-plugin-cpy'
+import html from 'rollup-plugin-bundle-html'
+import serve from 'rollup-plugin-serve'
 
-const config = (browser, minify) => {
+const isWatch = process.argv.includes('-w') || process.argv.includes('--watch')
+const isProduction = process.env.NODE_ENV === 'production'
 
-  const plugins = [
-    typescript({target: browser ? 'es3': 'es6'}),
+const config = (format, file, minify, server = false) => ({
+  input: './ts/fireworks.ts',
+  output: {
+    name: 'Fireworks',
+    file,
+    format,
+    amd: {id: 'Fireworks'}
+  },
+  plugins: [
+    typescript({target: format === 'umd' ? 'es3': 'es6'}),
     copy({
       files: ['./fireworks.d.ts'],
-      dest: 'dist'
+      dest: './dist/'
+    }),
+    html({
+      template: './html/index.html',
+      filename: 'index.html',
+      dest: './dist/',
+      inject: 'head',
+      ignore: /min|node/g,
+      externals: [{
+        file: './dist/example.css',
+        type: 'css'
+      },{
+        file: './dist/example.js',
+        type: 'js',
+        pos: 'after'
+      }]
+    }),
+    resolve({
+      extensions: ['.js', '.mjs']
+    }),
+    minify && terser(),
+    server && serve({
+      port: 8001,
+      host: '0.0.0.0',
+      path: 'examples/index.html',
+      contentBase: ['dist', 'examples'],
+      open: false,
+      wait: 500
     })
   ]
-
-  if (browser) {
-    plugins.push(
-      resolve({
-        extensions: ['.js', '.mjs']
-      })
-    )
-  }
-
-  if (minify) {
-    plugins.push(terser())
-  }
-
-  return {
-    input: './ts/fireworks.ts',
-    output: browser ? {
-      file: `dist/fireworks${minify ? '.min' : ''}.js`,
-      name: 'Fireworks',
-      format: 'umd',
-      amd: {id: 'Fireworks'}
-    } : [{
-      file: './dist/fireworks.mjs',
-      format: 'es'
-    }, {
-      file: './dist/fireworks.node.js',
-      format: 'cjs'
-    }],
-    plugins
-  }
-}
+})
 
 export default [
-  config(true, false),  // browser.unmin
-  config(true, true),   // browser.min
-  config(false)         // nodejs (mjs, cjs)
+  config('umd', './dist/fireworks.js', false, !isProduction && isWatch),
+  config('umd', './dist/fireworks.min.js', true),
+  config('module', './dist/fireworks.mjs', false),
+  config('cjs', './dist/fireworks.node.js', false)
 ]
