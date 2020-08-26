48c8a8b66a96d03a50c3af4e067bbc7793447cb6
Tyler Waters <tyler.waters@gmail.com>
2020-08-25 22:03:55 -0700

    Update dependencies
    Includes switching to use rollup-plugin-html2 because the other one
    doesn't work anymore under latest rollup

--- rollup.config.js
+++ rollup.config.js
@@ -2,63 +2,69 @@ import resolve from 'rollup-plugin-node-resolve'
 import { terser } from 'rollup-plugin-terser'
 import typescript from 'rollup-plugin-typescript'
 import copy from 'rollup-plugin-cpy'
-import html from 'rollup-plugin-bundle-html'
+import html from 'rollup-plugin-html2'
 import serve from 'rollup-plugin-serve'
 
 const isWatch = process.argv.includes('-w') || process.argv.includes('--watch')
-const isProduction = process.env.NODE_ENV === 'production'
+const isProduction = process.env.NODE_ENV !== 'development'
 
-const config = (format, file, minify, server = false) => ({
+const config = ({
+  format,
+  file,
+  minify,
+  useServer = false,
+  useHtml = false,
+}) => ({
   input: './ts/fireworks.ts',
   output: {
     name: 'Fireworks',
     file,
     format,
-    amd: { id: 'Fireworks' }
+    amd: { id: 'Fireworks' },
   },
   plugins: [
     typescript({ target: format === 'umd' ? 'es3' : 'es6' }),
     copy({
-      files: ['./fireworks.d.ts'],
-      dest: './dist/'
-    }),
-    html({
-      template: './html/index.html',
-      filename: 'index.html',
+      files: ['./fireworks.d.ts'].concat(
+        isProduction ? [] : ['./examples/example.css', './examples/example.js']
+      ),
       dest: './dist/',
-      inject: 'head',
-      ignore: /min|node/g,
-      externals: [
-        {
-          file: './dist/example.css',
-          type: 'css'
-        },
-        {
-          file: './dist/example.js',
-          type: 'js',
-          pos: 'after'
-        }
-      ]
     }),
+    useHtml &&
+      html({
+        template: './html/index.html',
+        fileName: 'index.html',
+        inject: 'head',
+      }),
     resolve({
-      extensions: ['.js', '.mjs']
+      extensions: ['.js', '.mjs'],
     }),
     minify && terser(),
-    server &&
+    useServer &&
       serve({
         port: 8001,
         host: '0.0.0.0',
         path: 'examples/index.html',
         contentBase: ['dist', 'examples'],
         open: false,
-        wait: 500
-      })
-  ]
+        wait: 500,
+      }),
+  ],
 })
 
 export default [
-  config('umd', './dist/fireworks.js', false, !isProduction && isWatch),
-  config('umd', './dist/fireworks.min.js', true),
-  config('module', './dist/fireworks.mjs', false),
-  config('cjs', './dist/fireworks.node.js', false)
+  config({
+    format: 'umd',
+    file: './dist/fireworks.js',
+    minify: false,
+    useServer: !isProduction && isWatch,
+  }),
+  config({
+    format: 'umd',
+    file: './dist/fireworks.min.js',
+    minify: true,
+    useHtml: true,
+  }),
+  config({ format: 'module', file: './dist/fireworks.mjs', minify: false }),
+  config({ format: 'cjs', file: './dist/fireworks.node.js', minify: false }),
 ]
